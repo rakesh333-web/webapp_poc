edc834b7325a21259a6692a591ff0dc0f5696730
rakesh333-web <85956046+rakesh333-web@users.noreply.github.com>
2024-09-10 11:20:27 +0530

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -3,7 +3,7 @@ import streamlit
 from openai import AzureOpenAI
     
 client = AzureOpenAI(
-  azure_endpoint = os.getenv("https://genainorthcentralus.openai.azure.com/"), 
+  azure_endpoint = "https://genainorthcentralus.openai.azure.com/", 
   api_key=os.getenv("b5d5b5ea2ebd471b88b631a34ab7d522"),  
   api_version="2024-02-01"
 )
@@ -13,5 +13,14 @@ deployment_name='gpt-4odeployment'
 
 st.title("GPT Responce")
 start_phrase = 'code for the using PS instrument, force all pins to 0V and then Force -0.010 mA to the pin under test LED2 and measure the voltage (MV) '
-response = client.completions.create(model=deployment_name, prompt=start_phrase, max_tokens=10) 
+response = client.completions.create(
+    model=deployment_name,
+    prompt=start_phrase,
+    max_tokens=100,
+    top_p=0.5,
+    frequency_penalty=0,
+    presence_penalty=0,
+    best_of=1,
+    stop=None
+) 
 st.write(response.choices[0].text)
