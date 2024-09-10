22136b0b738a1bcd61f7d12bd73d35668196ba41
rakesh333-web <85956046+rakesh333-web@users.noreply.github.com>
2024-09-10 22:47:16 +0530

    Update rakesh_chat.py

--- rakesh_chat.py
+++ rakesh_chat.py
@@ -1,42 +1,7 @@
 import os
 import requests
 import base64
- 
-# Configuration
-API_KEY = "b5d5b5ea2ebd471b88b631a34ab7d522"
-IMAGE_PATH = "YOUR_IMAGE_PATH"
-encoded_image = base64.b64encode(open(IMAGE_PATH, 'rb').read()).decode('ascii')
-headers = {
-    "Content-Type": "application/json",
-    "api-key": API_KEY,
-}
- 
-# Payload for the request
-payload = {
-  "messages": [
-    {
-      "role": "system",
-      "content": [
-        {
-          "type": "text",
-          "text": "You are an AI assistant that helps people find information."
-        }
-      ]
-    }
-  ],
-  "temperature": 0.7,
-  "top_p": 0.95,
-  "max_tokens": 800
-}
- 
-ENDPOINT = "https://genainorthcentralus.openai.azure.com/openai/deployments/gpt4/chat/completions?api-version=2024-02-15-preview"
- 
-# Send request
-try:
-    response = requests.post(ENDPOINT, headers=headers, json=payload)
-    response.raise_for_status()  # Will raise an HTTPError if the HTTP request returned an unsuccessful status code
-except requests.RequestException as e:
-    raise SystemExit(f"Failed to make the request. Error: {e}")
- 
-# Handle the response as needed (e.g., print or process)
-print(response.json())
+import streamlit as st
+
+st.title("gpt responce")
+
