46982a0167dc5cb1bea18485dcc1ba59cffc78c2
rakesh333-web <85956046+rakesh333-web@users.noreply.github.com>
2024-09-11 20:04:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,35 +28,40 @@ def process_client(df):
     options = df['english sentence'].tolist()
     z = st.radio('Select a sentence:', options)
     API_KEY = "b5d5b5ea2ebd471b88b631a34ab7d522"
-
-    # Define the headers for the API request
     headers = {
         "Content-Type": "application/json",
         "api-key": API_KEY,
     }
     
-    # Define the payload for the API request
+    # Payload for the request
     payload = {
-        "messages": [
+      "messages": [
+        {
+          "role": "system",
+          "content": [
             {
-                "role": "system",
-                "content": [
-                    {
-                        "type": "text",  # Ensure 'type' is correctly defined as 'text'
-                        "text": z
-                    }
-                ]
+              "type": "text",
+              "text": "You are an AI assistant for code generation. "
             }
-        ],
-        "temperature": 0.7,
-        "top_p": 0.95,
-        "max_tokens": 800
+          ]
+        },
+        {
+          "role": "user",
+          "content": [
+            {
+              "type": "text",
+              "text":  z
+            }
+          ]
+        },
+      "temperature": 0.7,
+      "top_p": 0.95,
+      "max_tokens": 800
     }
     
-    # Define the API endpoint
     ENDPOINT = "https://genainorthcentralus.openai.azure.com/openai/deployments/gpt-4odeployment/chat/completions?api-version=2024-02-15-preview"
     
-    # Make the API request
+    # Send request
     try:
         response = requests.post(ENDPOINT, headers=headers, json=payload)
         response.raise_for_status()  # Will raise an HTTPError if the HTTP request returned an unsuccessful status code
@@ -64,9 +69,10 @@ def process_client(df):
         raise SystemExit(f"Failed to make the request. Error: {e}")
     
     # Handle the response as needed (e.g., print or process)
-    result = response.json()
+    result=response.json()
     content = result.get('choices', [{}])[0].get('message', {}).get('content', 'No content found')
     return content
+    
    
 
 def main():
