77b4deda417094c9f78401aa1446ed01cc6f29ba
rakesh333-web <85956046+rakesh333-web@users.noreply.github.com>
2024-09-11 14:36:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def process_client(df):
                 ]
             }
         ],
-        "temperature": 0.01,
+        "temperature": 0,
         "top_p": 0.95,
         "max_tokens": 800
     }
